e20ea41237cc9ca30bc580779db3e3d6c1d9c6f1
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2021-10-06 14:14:44 -0300

    tweetifyText

--- src/net/Mega2223/utils/genericTools.java
+++ src/net/Mega2223/utils/genericTools.java
@@ -1,6 +1,11 @@
 package net.Mega2223.utils;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class genericTools {
+
     public static String ConvertToHTML(String[] eee) {
         String end = "";
         for (int ever = 0; ever < eee.length; ever++) {
@@ -16,5 +21,25 @@ public class genericTools {
 
         }
         return end;
+    }
+    public static List<String> tweetifyText(String textToSplitify, int maxLength) {
+        List<String> finalTweets = new ArrayList<String>();
+        String[] words = textToSplitify.split(" ");
+        String actualTweet = "";
+
+        for(int q = 0; q < words.length; q++){
+            if((actualTweet + words[q]).length() > maxLength){ //280
+                finalTweets.add(actualTweet);
+                actualTweet = words[q] + " ";
+            } else {actualTweet = actualTweet + words[q] + " ";}
+
+        }
+
+
+        //System.out.println("\n\n\n" + finalTweets.get(q));
+        // eu poderia fazer um sistema de CopiaCola mas ia dar muito trabalho
+        // e no final o objetivo de fazer isso é pra evitar trabalho
+
+        return finalTweets;
     }
 }
